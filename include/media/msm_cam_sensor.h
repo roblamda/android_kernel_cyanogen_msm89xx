caef2c24c1939cba19cc70d85a33e60d9fe915d7
Arvin Quilao <arquilao@gmail.com>
2018-01-11 08:26:03 +0000

    msm: camera: Import Seed camera driver
    This Driver for Andromax EC (C46B2H), This Driver From Android One 2 Gen (Seed)
    
    Fvck for front flashlight
    
    Change-Id: I5ddff95ea74e9ecf513a7c5c451a183c76479ad3
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -1,3 +1,11 @@
+
+#ifdef CONFIG_MACH_WT86518
+#include "msm_cam_sensor_wt86518.h"
+#else
+#ifdef CONFIG_SEED_CAMERA
+#include <media/msm_cam_sensor_seed.h>
+#else
+
 #ifndef __LINUX_MSM_CAM_SENSOR_H
 #define __LINUX_MSM_CAM_SENSOR_H
 
@@ -784,3 +792,8 @@ struct msm_flash_cfg_data_t32 {
 #endif
 
 #endif /* __LINUX_MSM_CAM_SENSOR_H */
+<<<<<<< HEAD
+=======
+#endif /* CONFIG_MACH_WT86518 */
+#endif /* CONFIG_SEED_CAMERA */
+>>>>>>> d746e47259a... msm: camera: Import Seed camera driver
--- include/media/msmb_isp.h
+++ include/media/msmb_isp.h
@@ -9,6 +9,14 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#ifdef CONFIG_MACH_WT86518
+#include "msmb_isp_wt86518.h"
+#else
+
+#ifdef CONFIG_SEED_CAMERA
+#include <media/msmb_isp_seed.h>
+#else
+
 #ifndef __MSMB_ISP__
 #define __MSMB_ISP__
 
@@ -639,3 +647,9 @@ struct msm_isp_event_data32 {
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp_event_data)
 #endif
 #endif /* __MSMB_ISP__ */
+<<<<<<< HEAD
+=======
+#endif /* CONFIG_MACH_WT86518 */
+#endif /* CONFIG_SEED_CAMERA */
+
+>>>>>>> d746e47259a... msm: camera: Import Seed camera driver
