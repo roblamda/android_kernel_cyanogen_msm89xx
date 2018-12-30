d95fc93d39d4dc7c6d0beffcbb9ec6e5794d219f
H34THCL1FF <kayabaakihikos4o@gmail.com>
2018-12-30 07:15:36 +0700

    Kang Impulse Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -443,6 +443,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BACON)
 extern struct cpufreq_governor cpufreq_gov_bacon;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bacon)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
